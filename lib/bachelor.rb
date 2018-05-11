309c545980ccf990a7a6be56b0c5d795a4fab9d6
malorie16 <malorie18@gmail.com>
2018-05-11 19:28:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,32 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |facts|
+      if facts["status"] == "Winner"
+        return facts["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, stats|
+    data[season].each do |facts|
+      binding.pry
+      if facts["occupation"] == occupation
+        return facts["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.each do |season, stats|
+    data[season].each do |facts|
+          binding.pry
+      return facts.count(hometown)
+
+      end
+  end
 end
 
 def get_occupation(data, hometown)

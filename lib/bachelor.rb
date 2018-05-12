a8c3d8008a125bb3bffa6d072608d3cf517ec6f3
github username <github email address>
2018-05-12 16:10:02 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,51 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |facts|
+      if facts["status"] == "Winner"
+        return facts["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, stats|
+    data[season].each do |facts|
+      if facts["occupation"] == occupation
+        return facts["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |season, stats|
+    stats.each do |facts|
+      if facts["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
+  i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |stats|
+      if stats["hometown"] == hometown
+        return stats["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  num_of_ppl = 0
+  total_ages = 0
+  data[season].each do |contestants|
+        total_ages += contestants["age"].to_i
+        num_of_ppl += 1
+  end
+    (total_ages.to_f / num_of_ppl).round
 end
